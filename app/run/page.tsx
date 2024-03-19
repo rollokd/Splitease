37f51df523569965e95568bf6ad02185ee0d3337
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-03-19 13:41:38 -0500

    Fix: gabe Commented out

--- app/run/page.tsx
+++ app/run/page.tsx
@@ -3,22 +3,22 @@ import { GroupCard } from "@/components/ui/group-card";
 import { GroupChart } from "@/components/ui/bar-chart";
 import { fetchGroupTotals, fetchUserTransactionsAndGroups, getUsersbyGroup} from "@/lib/data";
 
-export async function gabe () {
-  const userID: string = '9ec739f9-d23b-4410-8f1a-c29e0431e0a6';
-  const groupID: string = '5909a47f-9577-4e96-ad8d-7af0d52c3267';
-  try {
-    // const userTransactions = await fetchUserTransactionsAndGroups();
-    const groupTotals = await fetchGroupTotals(userID, groupID);
-    const userByGroup = await getUsersbyGroup(groupID);
+// export async function gabe () {
+//   const userID: string = '9ec739f9-d23b-4410-8f1a-c29e0431e0a6';
+//   const groupID: string = '5909a47f-9577-4e96-ad8d-7af0d52c3267';
+//   try {
+//     // const userTransactions = await fetchUserTransactionsAndGroups();
+//     const groupTotals = await fetchGroupTotals(userID, groupID);
+//     const userByGroup = await getUsersbyGroup(groupID);
 
-    // console.log('here sebA userTransactions: ', userTransactions);
-    console.log('here sebA: groupTotals', groupTotals);
-    console.log('here sebA usersbyGroup: ', userByGroup);
-    //loop 
-  } catch (error) {
-    console.error('Error querying the database:', error);
-  }
-}
+//     // console.log('here sebA userTransactions: ', userTransactions);
+//     console.log('here sebA: groupTotals', groupTotals);
+//     console.log('here sebA usersbyGroup: ', userByGroup);
+//     //loop 
+//   } catch (error) {
+//     console.error('Error querying the database:', error);
+//   }
+// }
 export default async function  Home() {
   const userID: string = '9ec739f9-d23b-4410-8f1a-c29e0431e0a6';
   const groupID: string = '5909a47f-9577-4e96-ad8d-7af0d52c3267';
@@ -31,7 +31,6 @@ export default async function  Home() {
     <div>
       <GroupCard groupID={groupID} userID={userID} />
     </div>
-    {gabe()}
     </>
   );
 }
