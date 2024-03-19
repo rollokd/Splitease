80fb177be7d997a0fe0b494880ffba94572ce74e
Rollo Kennedy-Dyson <rollokd@gmail.com>
2024-03-19 15:09:13 +0000

    feat: group components

---
+++ components/group-view/group-balances/group-balances.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const groupBalances = (props: Props) => {
+  // TODO: fetch all individual balances for the group
+
+  return <div>groupBalances</div>;
+};
+
+export default groupBalances;
