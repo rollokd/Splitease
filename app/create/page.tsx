715b39ba8ceb54934c4a2616db1ff9ab26b5a10a
ByteBlink <jaakupienes@gmail.com>
2024-03-21 17:17:43 +0100

    feat: create page w/ its components

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import CreateGroupForm from "../../components/groups/create-form";
-import { fetchUsers } from "@/lib/data";
+import React from 'react';
+import CreateGroupForm from '../../components/groups/create-form';
+import { fetchUsers } from '@/lib/data';
+import GroupCrumbs from '@/components/group-view/breadcrumbs';
 
 const page = async () => {
+  const name = 'Create';
   const users = await fetchUsers();
-  return <CreateGroupForm users={users} />;
+  return (
+    <div className='flex flex-col p-3 gap-3 h-full last:mt-auto'>
+      <strong>
+        <GroupCrumbs name={name} />
+      </strong>
+      <CreateGroupForm users={users} />;
+    </div>
+  );
 };
 
 export default page;
