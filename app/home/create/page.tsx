2ade5c7113129ebdc613dd1eece8787bae6794cd
Rollo Kennedy-Dyson <rollokd@gmail.com>
2024-03-22 12:36:43 +0000

    fix: semi colon

--- app/home/create/page.tsx
+++ app/home/create/page.tsx
@@ -1,20 +1,20 @@
-import GroupCrumbs from '@/components/group-view/breadcrumbs';
-import React from 'react';
-import CreateGroupForm from '@/components/groups/create-form';
-import { fetchUsers } from '@/lib/data';
-import { getUserId } from '@/lib/actions';
+import GroupCrumbs from "@/components/group-view/breadcrumbs";
+import React from "react";
+import CreateGroupForm from "@/components/groups/create-form";
+import { fetchUsers } from "@/lib/data";
+import { getUserId } from "@/lib/actions";
 
 const page = async () => {
-  const name = 'Create';
+  const name = "Create";
   const users = await fetchUsers();
   const userID = (await getUserId()) as string;
 
   return (
-    <div className='flex flex-col p-3 gap-3 h-full last:mt-auto'>
-      <strong className='gap-3 mb-4 mt-2 p-3 '>
-        <GroupCrumbs name={name} type={'create'} />
+    <div className="flex flex-col p-3 gap-3 h-full last:mt-auto">
+      <strong className="gap-3 mb-4 mt-2 p-3 ">
+        <GroupCrumbs name={name} type={"create"} />
       </strong>
-      <CreateGroupForm users={users} userID={userID} />;
+      <CreateGroupForm users={users} userID={userID} />
     </div>
   );
 };
