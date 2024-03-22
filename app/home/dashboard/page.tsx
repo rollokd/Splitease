2c11b66886655b99f178666db833c44341725c8f
Gabe Mata <gabemata@gmail.com>
2024-03-22 22:04:09 +0100

    feat: dark mode

--- app/home/dashboard/page.tsx
+++ app/home/dashboard/page.tsx
@@ -14,6 +14,7 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 import { createGroup, getUserId } from '@/lib/actions';
 import { moneyFormat } from '@/lib/utils';
 import { redirect } from 'next/navigation';
+import { ModeToggle } from '@/components/themeMode';
 
 export default async function Home() {
   const userID = await getUserId();
@@ -56,6 +57,7 @@ export default async function Home() {
             await signOut();
           }}
         >
+          <ModeToggle />
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,15 +10,18 @@ export const metadata: Metadata = {
   description: "Bill splitting made easy",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({  children,}: Readonly<{  children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body className={inter.className + "flex flex-col h-screen box-border"}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
