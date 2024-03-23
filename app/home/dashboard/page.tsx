8273cfe6c78293bfe1812fed180f17988d52668c
Gabe Mata <gabemata@gmail.com>
2024-03-23 18:30:36 +0100

    feat styling

--- app/home/dashboard/page.tsx
+++ app/home/dashboard/page.tsx
@@ -1,10 +1,7 @@
-export const dynamic = "force-dynamic"
+export const dynamic = 'force-dynamic';
 import { GroupCard } from '@/components/group-card';
 import { GroupChart } from '../../../components/bar-chart';
-import {
-  getUserGroups,
-  fetchUserBalance,
-} from '../../../lib/data';
+import { getUserGroups, fetchUserBalance } from '../../../lib/data';
 import Totals from '../../../components/Totals';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
@@ -42,46 +39,58 @@ export default async function Home() {
   //const bears = useStore((state) => state.bears);
   return (
     <>
-      <div className="p-4">
-        <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
-        >
-          <ModeToggle />
-          <Button className='ml-4'><PowerIcon className="w-4" />   <div className="ml-2"> Sign Out</div> </Button>
-        </form>
+      <div className="mb-2">
+        <div className="p-4">
+          <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <ModeToggle />
+            <Button className="ml-4">
+              <PowerIcon className="w-4" />{' '}
+              <div className="ml-2"> Sign Out</div>{' '}
+            </Button>
+          </form>
+        </div>
         <Totals userId={userID} />
 
-      <div className="m-4 flex justify-end">
-        <Button >
-          <Link href="/home/create">Create Group +</Link>
-        </Button>
-      </div>
-      <div style={{ height: "400px", overflowY: "auto", borderColor: "var(--card)", borderRadius: "5px", borderStyle: 'ridge', borderWidth: "1px"}}>
-        {userID &&
-          groups.map((group) => (
-            <Link key={group.group_id} href={`/home/group/${group.group_id}`}>
-              <GroupCard
-                key={group.group_id}
-                group_id={group.group_id}
-                user_id={userID}
-              />
-            </Link>
-          ))}
-      </div>
-      <div className='flex justify-center pt-5'>
-        <GroupChart data={balances}></GroupChart>
-      </div>
-      <div className='flex justify-center m-4 pt-1 pb-3'>
-        <Link className="w-full" href={`/home/settle_up_dashboard`}>
-          <Button className='bg-green-500 w-full'>
-            Settle Up
+        <div className="m-4 flex justify-end">
+          <Button>
+            <Link href="/home/create">Create Group +</Link>
           </Button>
-        </Link>
+        </div>
+        <div
+          style={{
+            height: '400px',
+            overflowY: 'auto',
+            borderColor: 'var(--card)',
+            borderRadius: '5px',
+            borderStyle: 'ridge',
+            borderWidth: '1px'
+          }}
+        >
+          {userID &&
+            groups.map((group) => (
+              <Link key={group.group_id} href={`/home/group/${group.group_id}`}>
+                <GroupCard
+                  key={group.group_id}
+                  group_id={group.group_id}
+                  user_id={userID}
+                />
+              </Link>
+            ))}
+        </div>
+        <div className="flex justify-center pt-5">
+          <GroupChart data={balances}></GroupChart>
+        </div>
+        <div className="flex justify-center m-4 pt-1 pb-3">
+          <Link className="w-full" href={`/home/settle_up_dashboard`}>
+            <Button className="bg-green-500 w-full">Settle Up</Button>
+          </Link>
+        </div>
       </div>
-    </div>
     </>
   );
 }
