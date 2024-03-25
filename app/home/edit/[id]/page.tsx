d281ab14fccbb3ca768749e556a068dced39f573
ByteBlink <jaakupienes@gmail.com>
2024-03-25 03:10:14 +0100

    fix: balances

--- app/home/edit/[id]/page.tsx
+++ app/home/edit/[id]/page.tsx
@@ -16,9 +16,13 @@ async function Page({ params }: Props) {
   const users = await fetchUsers();
   const groupUsers = await getUsersbyGroup(group_id);
   const userID = (await getUserId()) as string;
-  const balances = await Promise.all(
-    groupUsers.map((user_id) => fetchUserBalance(user_id.user_id, group_id))
+  const balancesPromises = groupUsers.map((user) =>
+    fetchUserBalance(user.user_id, group_id).then((balance) => ({
+      [user.user_id]: balance,
+    }))
   );
+  const balancesObjects = await Promise.all(balancesPromises);
+  const balances = Object.assign({}, ...balancesObjects);
   return (
     <div className='flex flex-col p-3 gap-3 h-full last:mt-auto'>
       <strong className='gap-3 mb-4 mt-2 p-3'>
