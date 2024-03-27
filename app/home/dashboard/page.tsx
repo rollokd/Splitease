d299797ea77ca096284de3b58ac0ed42521da33e
Gabe Mata <gabemata@gmail.com>
2024-03-27 17:05:20 +0100

    feat: Dynamic rendering pay or receive

--- app/home/dashboard/page.tsx
+++ app/home/dashboard/page.tsx
@@ -1,24 +1,14 @@
-export const dynamic = "force-dynamic";
-// import { GroupCard } from '@/components/group-card-test';
-import { GroupCard } from "@/components/group-card";
-import { GroupChart } from "../../../components/bar-chart";
-import {
-  getUserGroups,
-  fetchUserBalance,
-  getUsersbyGroup,
-} from "../../../lib/data";
-import Totals from "../../../components/Totals";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { signOut, auth } from "@/auth";
-import { PowerIcon } from "@heroicons/react/24/outline";
-import { getUserId } from "@/lib/actions";
-import { moneyFormat } from "@/lib/utils";
-import { ModeToggle } from "@/components/themeMode";
-import { fetchOneUserBalanceForGroup } from "@/lib/databaseFunctions/fetchOneUserBalanceForGroup";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { UserWJunction } from "@/lib/definititions";
-import { redirect } from "next/navigation";
+export const dynamic = 'force-dynamic';
+import { GroupCard } from '@/components/group-card';
+import { getUserGroups, getUsersbyGroup } from '../../../lib/data';
+import Totals from '../../../components/Totals';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { getUserId } from '@/lib/actions';
+import { fetchOneUserBalanceForGroup } from '@/lib/databaseFunctions/fetchOneUserBalanceForGroup';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { UserWJunction } from '@/lib/definititions';
+import { redirect } from 'next/navigation';
 
 const getUsers = (userByGroup: UserWJunction[]) => {
   const firstnames = userByGroup.map((user) => user.firstname);
@@ -31,11 +21,11 @@ export default async function Home() {
   try {
     userID = await getUserId();
   } catch (error) {
-    console.error("Failed to fetch userID:", error);
+    console.error('Failed to fetch userID:', error);
   }
   if (userID === undefined) {
-    console.error("Failed to fetch userID");
-    redirect("login");
+    console.error('Failed to fetch userID');
+    redirect('login');
   }
 
   let userGroups = await getUserGroups(userID);
@@ -49,23 +39,27 @@ export default async function Home() {
       }
       const userByGroup = await getUsersbyGroup(group.group_id);
       const listOfUsers = getUsers(userByGroup || []);
-      return { name: group.name, shortName: shortName, total: rows[0].lent_amount - rows[0].owed_amount, listOfUsers: listOfUsers, group_id: group.group_id };
+      return {
+        name: group.name,
+        shortName: shortName,
+        total: rows[0].lent_amount - rows[0].owed_amount,
+        listOfUsers: listOfUsers,
+        group_id: group.group_id
+      };
     })
   );
-  // console.log('groupBalances results from dashboard page: ', groupBalances);
 
   return (
-    <div className='mt-4'>
+    <div className="mt-4">
       <Totals userId={userID} />
 
-      
       <div className="m-4 flex justify-end">
         <Button>
           <Link href="/home/create">Create Group +</Link>
         </Button>
       </div>
       <Card className="border-none shadow-none">
-        <CardHeader className='mb-4'>
+        <CardHeader className="mb-4">
           <CardTitle>Groups</CardTitle>
         </CardHeader>
 
@@ -74,16 +68,15 @@ export default async function Home() {
             style={{
               height: '400px',
               overflowY: 'auto'
-              // borderColor: 'var(--card)',
-              // borderRadius: '5px',
-              // borderStyle: 'ridge',
-              // borderWidth: '1px'
             }}
           >
             <div>
               {userID &&
-                groupBalances.map(( group ) => (
-                  <Link key={group.group_id} href={`/home/group/${group.group_id}`}>
+                groupBalances.map((group) => (
+                  <Link
+                    key={group.group_id}
+                    href={`/home/group/${group.group_id}`}
+                  >
                     <GroupCard
                       key={group.group_id}
                       groupName={group.name}
@@ -93,24 +86,8 @@ export default async function Home() {
                   </Link>
                 ))}
             </div>
-            {/* <div>
-              {userID &&
-                groupBalances.map(( group ) => (
-                  <Link key={group.group_id} href={`/home/group/${group.group_id}`}>
-                    <GroupCard
-                      key={group.group_id}
-                      groupName={group.name}
-                      groupTotals={Number(group.total)}
-                      listOfUsers={group.listOfUsers}
-                    />
-                  </Link>
-                ))}
-            </div> */}
           </div>
         </CardContent>
-        {/* <div className='flex justify-center pt-5 mt-5 mb-10'>
-          {userID && <GroupChart data={groupBalances}></GroupChart>}
-        </div> */}
         <div className="flex justify-center m-4 pt-1 pb-3 fixed inset-x-0 bottom-0">
           <Link className="w-full" href={`/home/settle_up_dashboard`}>
             <Button className="w-full">Settle Up</Button>
--- components/Totals.tsx
+++ components/Totals.tsx
@@ -1,9 +1,13 @@
 import CardTotals from './cardTotals';
 import { fetchOwnDashboardData } from '../lib/data';
 import { moneyFormat } from '@/lib/utils';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
-import { Button } from './ui/button';
-import { Separator } from './ui/separator';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from './ui/card';
 import Link from 'next/link';
 
 export default async function Totals({ userId }: { userId: string }) {
@@ -15,43 +19,53 @@ export default async function Totals({ userId }: { userId: string }) {
   }
   let paidbyMeMoney = own?.paidbyMe;
   let myPortionOfBillsMoney = own?.myPortionOfBills;
-  let totalMoney = own?.total;
+  let totalMoney = own?.total ?? 0;
+
+  let myTitle;
+  if (Math.sign(totalMoney) === -1) {
+    myTitle = 'Pay';
+  } else {
+    myTitle = 'Receive';
+  }
 
   return (
     <Card className="border-none shadow-none">
-      <CardHeader className='mb-4'>
+      <CardHeader className="mb-4">
         <CardTitle>Balances</CardTitle>
       </CardHeader>
-     
-      <CardContent className='m-2'>
-      <div className="flex flex-wrap gap-2 md:gap-4 lg:gap-6">
-        <div className="flex-1">
-          <CardTotals
-            myColor="text-green-500"
-            title="Owed / Collect"
-            amount={moneyFormat(paidbyMeMoney)}
-          />
-        </div>
-        <div className="flex-1">
-          <CardTotals
-            myColor="text-red-500"
-            title="Owe / Debt"
-            amount={moneyFormat(myPortionOfBillsMoney)}
-          />
-        </div>
-        <div className="flex-1">
+
+      <CardContent className="m-2">
+        <div className="flex flex-wrap gap-2 md:gap-4 lg:gap-6">
           <div className="flex-1">
             <CardTotals
-              myColor=""
-              title="+ Get - Pay "
-              amount={moneyFormat(totalMoney)}
+              myColor="text-green-500"
+              title="Owed / Collect"
+              amount={moneyFormat(paidbyMeMoney)}
             />
           </div>
+          <div className="flex-1">
+            <CardTotals
+              myColor="text-red-500"
+              title="Owe / Debt"
+              amount={moneyFormat(myPortionOfBillsMoney)}
+            />
+          </div>
+          <div className="flex-1">
+            <div className="flex-1">
+              <CardTotals
+                myColor=""
+                title={myTitle}
+                amount={moneyFormat(totalMoney)}
+              />
+            </div>
+          </div>
         </div>
-      </div>
       </CardContent>
-      <CardFooter><Link href="/home/analytics" className='underline ml-4'>Analytics</Link></CardFooter> 
-      
+      <CardFooter>
+        <Link href="/home/analytics" className="underline ml-4">
+          Analytics
+        </Link>
+      </CardFooter>
     </Card>
   );
 }
