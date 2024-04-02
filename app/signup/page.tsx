f0917effe5a58497d27b471ec708ab44377d50d3
ByteBlink <jaakupienes@gmail.com>
2024-04-02 19:28:09 +0200

    fix: remove redirect

---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignForm from "@/components/ui/sign-up-form";
+
+export default function Page() {
+  return (
+    <main className='flex items-center justify-center pt-[20vh]'>
+      <SignForm />
+    </main>
+  );
+}
